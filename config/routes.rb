90fe2cc720074cc8b5f19768e1b239a77a3711cd
naoki0925 <naoking250912>
2021-02-17 11:13:20 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#top'
-  get "home/about" => "users#about"
+  get "home/about" => "homes#about"
   resources :users, only: [:new, :show, :index, :create, :edit, :update]
   resources :books, only: [:new, :show, :index, :create, :edit, :update, :destroy]
 end
