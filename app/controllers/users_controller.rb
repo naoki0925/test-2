6108b795eb2c7907c9604f0526411eec74692f86
naoki0925 <naoking250912>
2021-03-04 12:26:04 +0000

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.followings
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#top'
   get "home/about" => "homes#about"
-  resources :users, only: [:new, :show, :index, :create, :edit, :update]
+  resources :users, only: [:new, :show, :index, :create, :edit, :update]do
+    member do
+     get :following, :followers
+    end
+  end
   resources :books, only: [:new, :show, :index, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
